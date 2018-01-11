66e04750441d75e2e08a8e01d39ced0caccf9927
daniel <daniel@localbeats.com.au>
2018-01-11 10:20:49 +0900

    Updated localhost detector

--- wp-content/themes/wordpack/functions.php
+++ wp-content/themes/wordpack/functions.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Wordpack functions and definitions.
+ * TSS functions and definitions.
  *
- * @package Wordpack
- * @since Wordpack 0.1
+ * @package TSS
+ * @since TSS 0.1
  */
 
 require_once __DIR__ . '/inc/admin-functions.php';
@@ -14,7 +14,8 @@ require_once __DIR__ . '/inc/template-helpers.php';
 //
 // initialise
 
-if ( '127.0.0.1' == $_SERVER["REMOTE_ADDR"] ) {
+if (substr($_SERVER['REMOTE_ADDR'], 0, 4) == '127.' ||
+    $_SERVER['REMOTE_ADDR'] == '::1') {
   define( 'ENVIRONMENT', 'development' );
   ini_set( 'error_reporting', E_ALL );
   ini_set( 'display_errors', 'On' );
