428e81ff71f071092b38c58b189bf1919b427909
daniel <daniel@localbeats.com.au>
2018-01-12 09:45:49 +0900

    Added example function to WP template

--- wp-content/themes/wordpack/src/js/components/modules/WordpressTemplate.js
+++ wp-content/themes/wordpack/src/js/components/modules/WordpressTemplate.js
@@ -5,9 +5,15 @@ export default class WordpressTemplate extends Resizable {
     super();
 
     this.state = {
+      htmlBackgroundColor: "white",
       templateClasses: $("#template-root").attr("class")
     }
 
     this.registerResizeHandler();
+  }
+
+  setHtmlBackgroundColor = (hex) => {
+    this.state.htmlBackgroundColor = hex;
+    $("html").css("background-color", hex);
   }
 }
