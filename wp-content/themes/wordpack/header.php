1a9248a9965fead4a2276bb4f93ff402aacb9963
Daniel Courtness <danielcourtness@gmail.com>
2019-09-24 19:20:49 +0900

    Remove junk meta

--- wp-content/themes/wordpack/header.php
+++ wp-content/themes/wordpack/header.php
@@ -13,8 +13,7 @@
     https://courtness.netlify.com
     --><!--/noptimize-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
-    <meta name="description" content="Vera Babida">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" sizes="16x16 32x32" href="<?php echo site_url() ?>/favicon.ico">
 
